b3237011837e5324a6c667507158b8d9520383db
mohsenkamini <mohsenkamini@gmail.com>
2024-01-31 01:15:24 +0330

    the 2 and 3 commands seem to work

--- Final Project/Bank.java
+++ Final Project/Bank.java
@@ -1,3 +1,4 @@
+import java.beans.Customizer;
 import java.util.Scanner;
 
 class Menu {
@@ -31,6 +32,12 @@ class Menu {
                 case '6':
                     bank.handleCommand6(command.substring(2));
                     break;
+                case '7':
+                    bank.handleCommand7();
+                    break;
+                case '8':
+                    bank.handleCommand8(command.substring(2));
+                    break;
                 default:
                     i--;
                     break;
@@ -41,6 +48,11 @@ class Menu {
 
 public class Bank {
     LinkedList customers;
+    private String nameOf2; // max in - out
+    private float valueOf2; // max in - out
+
+    private String nameOf3; // min in - out
+    private float valueOf3; // min in - out
 
     public Bank() {
         this.customers = new LinkedList();
@@ -48,6 +60,67 @@ public class Bank {
 
     public static void main(String[] args) {
         Menu.main();
+    }
+    public void handleInOutDifference (Customer customer) {
+        customer.difference = customer.totalIn - customer.totalOut;
+        if (customer.difference > this.valueOf2){
+            this.valueOf2=customer.difference;
+            this.nameOf2=customer.name;
+        } else if (customer.difference == this.valueOf2 && this.nameOf2 != null){
+            if (customer.name.compareTo(this.nameOf2) < 0) {
+                this.valueOf2=customer.difference;
+                this.nameOf2=customer.name;
+            }
+        } else if (customer.name.equals(this.nameOf2)) {
+            this.nameOf2=null;
+            this.valueOf2=-1;
+        }
+
+        if (customer.difference < this.valueOf3){
+            this.valueOf3=customer.difference;
+            this.nameOf3=customer.name;
+        } else if (customer.difference == this.valueOf3 && this.nameOf3 != null){
+            if (customer.name.compareTo(this.nameOf3) < 0) {
+                this.valueOf3=customer.difference;
+                this.nameOf3=customer.name;
+            }
+        } else if (customer.name.equals(this.nameOf3)) {
+            this.nameOf3=null;
+            this.valueOf3=1;
+        }
+    }
+    public void calclateMinMax () {
+        float max=-1;
+        String maxName=null;
+        float min=1;
+        String minName=null;
+        Node index = customers.head;
+        for (int i=0 ; i < customers.size ; i++,index=index.next) {
+            if (max<index.customer.difference) {
+                maxName=index.customer.name;
+                max=index.customer.difference;
+            }
+            if (min>index.customer.difference) {
+                minName=index.customer.name;
+                min=index.customer.difference;
+            }
+        }
+        if (max > 0) {
+            this.valueOf2=max;
+            this.nameOf2=maxName;
+        }
+        else {
+            this.valueOf2=-1;
+            this.nameOf2=null;
+        }
+        if (min < 0){
+            this.valueOf3=min;
+            this.nameOf3=minName;
+        }
+        else {
+            this.valueOf2=1;
+            this.nameOf2=null;
+        }
     }
     public void handleCommand1(String input) {
         // Handle command 1
@@ -59,23 +132,26 @@ public class Bank {
 
         talabkar.totalOut += value;
         bedehkar.totalIn += value;
+        handleInOutDifference(talabkar);
+        handleInOutDifference(bedehkar);
 
         talabkar.accounts.insertFirst(nodes[1]).accountedValue += value;
         bedehkar.accounts.insertFirst(nodes[0]).accountedValue -= value;
-        System.out.println(customers.find(talabkar.name).name+" "+customers.find(bedehkar.name).name);
+        System.out.println(customers.find(talabkar.name).totalOut+" "+customers.find(bedehkar.name).totalOut);
     }
 
-    public String handleCommand2() {
+    public void handleCommand2() {
         // Handle command 2
-        String result = "";
-        customers.print();
-        return result;
+        if (this.valueOf2 < 0)
+            calclateMinMax();
+        System.out.println(this.nameOf2+this.valueOf2);
     }
 
-    public String handleCommand3() {
+    public void handleCommand3() {
         // Handle command 3
-        String result = "";
-        return result;
+        if (this.valueOf3 > 0)
+            calclateMinMax();
+        System.out.println(this.nameOf3+this.valueOf3);
     }
 
     public String handleCommand4(String input) {
@@ -96,4 +172,17 @@ public class Bank {
         String result = "";
         return result;
     }
+    public String handleCommand7() {
+        // Handle command 7
+        String result = "";
+        customers.print();
+        return result;
+    }
+    public String handleCommand8(String input) {
+        // Handle command 8
+        String result = "";
+        customers.find(input).accounts.print();
+        return result;
+    }
+
 }
--- Final Project/Customer.java
+++ Final Project/Customer.java
@@ -2,11 +2,13 @@ public class Customer {
     String name;
     float totalIn;
     float totalOut;
+    float difference;
 
     LinkedList accounts;
     float accountedValue; // this value is not global and will only be filled in accounts list.
     public Customer() {
         totalIn = 0.00f;
         totalOut = 0.00f;
+        difference = 0.00f;
     }
 }
